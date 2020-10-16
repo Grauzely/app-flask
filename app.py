fdfca5eb0ff4c857384a4d25cf71f64e7df85c4b
Gustavo Grauzely da Silva Alves <ggrauzely@gmail.com>
2020-10-16 18:40:21 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/teste")
+def index():
+    return 'Tentando CI/CD com Jenkins e Docker na EC2!'
+
+if __name__ == "__main__":
+    app.run()
