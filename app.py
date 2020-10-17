71da7ec6988ac0a0ba8069b34e733d9309123ebc
Gustavo Grauzely da Silva Alves <ggrauzely@gmail.com>
2020-10-17 11:42:54 -0400

    Deploy Perfeito, Agora vai :)

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/teste")
 def index():
-    return 'Tentando CI/CD com Jenkins e Docker na EC2! Deploy Perfeito'
+    return 'Tentando CI/CD com Jenkins e Docker na EC2! Deploy Perfeito, Agora vai :)'
 
 if __name__ == "__main__":
     app.run()
