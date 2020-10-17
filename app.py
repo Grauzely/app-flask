fa7b0fa11fba1fc2435af9c31184b8291662a38c
Gustavo Grauzely da Silva Alves <ggrauzely@gmail.com>
2020-10-17 10:44:29 -0400

    Teste Deploy Automatico

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/teste")
 def index():
-    return 'Tentando CI/CD com Jenkins e Docker na EC2! Teste Deploy'
+    return 'Tentando CI/CD com Jenkins e Docker na EC2! Teste Deploy Automatico'
 
 if __name__ == "__main__":
     app.run()
